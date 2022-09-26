eafa806e2d2b30d38a214ad66fee83643133a152
Emmanuel <iamemman18@yahoo.com>
2022-09-26 10:15:00 +0800

    First commit

---
+++ src/python1.py
@@ -0,0 +1,21 @@
+# The line below will print Hello Python!
+print("Hello Python!")
+
+# The line belo will print Hello
+print('Hello')
+
+# The line below will print "Hello"
+print('"Hello"')
+
+# The line below will print 'Hello'
+print("'Hello'")
+
+# The line below will print the following
+# Hello
+# from
+# python!
+print("""
+Hello
+from
+python
+""")
