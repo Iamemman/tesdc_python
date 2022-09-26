5ac971d678056309d4186a27bddd031b8b2d3c29
Emmanuel <iamemman18@yahoo.com>
2022-09-26 14:34:04 +0800

    commit pythonactivity1

---
+++ src/pythonactivity1.py
@@ -0,0 +1,7 @@
+name = input("Enter your First Name: ")
+name2 = input("Enter your Middle Name: " )
+name3 = input("Enter your Last Name: ")
+print("Hello " + name +" " + name2 + ". " + name3 )
+
+
+
