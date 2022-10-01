144ba224d680b4dfc2767452fdf35b4d5d0fb131
Emmanuel <iamemman18@yahoo.com>
2022-10-01 22:24:38 +0800

    commit pythonactivity2

---
+++ src/pythonactivity2.py
@@ -0,0 +1,14 @@
+# average grade program
+subject1 = int(input("Enter Grade in Math: "))
+subject2 = int(input("Enter Grade in English: "))
+subject3 = int(input("Enter Grade in Filipino: "))
+average = (subject1+subject2+subject3)/3
+x = average
+
+y = round( x )
+z = round(x,2)
+
+
+
+
+print(f'Average Grade is {z}')
